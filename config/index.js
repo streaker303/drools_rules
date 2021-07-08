35b3f971bc7e360ecade8f5240649c569d25beed
streaker303 <1241362854@qq.com>
2021-07-08 15:09:31 +0800

    dist

--- config/index.js
+++ config/index.js
@@ -81,7 +81,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
